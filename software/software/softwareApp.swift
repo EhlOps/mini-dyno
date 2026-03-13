a3878af9624565a61066c6a1d9e104983177d8b9
EhlOps <samtehlers@gmail.com>
2026-03-13 13:22:00 -0400

    Add software

---
+++ software/software/softwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  softwareApp.swift
+//  software
+//
+//  Created by Sam Ehlers on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct softwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
